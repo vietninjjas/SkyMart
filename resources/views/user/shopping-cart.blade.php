51bab278b40161b6b4a006e430a582e14646f44d
LinhLee <linhle24680@gmail.com>
2020-10-29 21:41:39 +0700

    chuyen giao dien index

--- resources/views/user/shopping-cart.blade.php
+++ resources/views/user/shopping-cart.blade.php
@@ -5,18 +5,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Trang Chủ</title>
-  <link href='./assets/images/favico.ico' rel='shortcut icon' type='image/ico'/>
+  <link href='assets/images/favico.ico' rel='shortcut icon' type='image/ico'/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-  <link rel="stylesheet" href="./assets/css/main.css">
-  <link rel="stylesheet" href="./assets/css/base.css">
-  <script src="./assets/js/js.js"></script>
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/base.css">
+  <script src="assets/js/js.js"></script>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./assets/font/fontawesome-free-5.14.0-web/css/all.min.css">
+  <link rel="stylesheet" href="assets/font/fontawesome-free-5.14.0-web/css/all.min.css">
 
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
   <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
