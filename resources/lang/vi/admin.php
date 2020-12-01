50ec7b625c72a5e202b7cb550ba35ca8e732ea13
vietninjjas <vietnq812@gmail.com>
2020-12-01 22:05:31 +0700

    thay doi trang thai:

--- resources/lang/vi/admin.php
+++ resources/lang/vi/admin.php
@@ -116,6 +116,7 @@ return [
         'order_status' => [
             'order_status' => 'Trạng thái',
             'wait' => 'Đang chờ',
+            'waitPaid' => 'Đang chờ, chưa thanh toán',
             'shippingNotPay' => 'Đang giao, chưa thanh toán',
             'shippingPaid' => 'Đang giao, đã thanh toán',
             'complete' => 'Hoàn thành',
