4e0003be6ef539152b35abb31f4278ebfda1e149
vietninjjas <vietnq812@gmail.com>
2020-12-06 01:09:24 +0700

    fix database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(WishlistTableSeed::class);
         $this->call(OrderTableSeed::class);
         $this->call(CheckoutTableSeed::class);
-        $this->call(FeedbackTableSeed::class);
         $this->call(BannerTableSeed::class);
         $this->call(DealTableSeed::class);
         $this->call(chillimageTableSeed::class);
